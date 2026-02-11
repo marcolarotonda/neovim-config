cee7cc18693db78c43600ba525e24669c1e6089b
Marco Larotonda <marcolarotonda@kubepartners.com>
2026-02-11 16:47:57 +0100

    fix ssh tunneling command

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -1,8 +1,13 @@
 local db_utils = require("utils.db")
-vim.api.nvim_create_user_command(
-    "DBSSH",
-    function(opts)
-        db_utils.create_ssh_tunnel(opts.args)
+vim.api.nvim_create_user_command("DBSSH", function(opts)
+    db_utils.create_ssh_tunnel(opts.args)
+end, {
+    nargs = 1,
+    desc = "Apre un tunnel SSH per il database specificato",
+    complete = function()
+        local databases = require("secrets.db")
+        return vim.tbl_map(function(v)
+            return v.db_alias
+        end, databases)
     end,
-    { nargs = 1, desc = "Apre un tunnel SSH per il database specificato" }
-)
+})
